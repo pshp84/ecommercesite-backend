12a7a61bc635ae3ab00f409c8c81fe56c5c9b051
amit <dev01.prishusoft@gmail.com>
2024-08-27 16:16:24 +0530

    added dialect

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  dialectModule: mysql2,
   logging: false,
   port: +process.env.DB_PORT!
 });
